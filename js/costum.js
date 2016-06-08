dded37a2c2bb4738d78e4603af788f6273dafaec
EduardoAAleixo <eduardo.a.aleixo@hotmail.com>
2016-06-08 17:31:05 +0100

    Projeto final

---
+++ js/costum.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	
+	$("#hambc").click(function(){
+		$("#hambl").slideToggle("slow");
+	});
+   
+    $("a[rel=molde]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("href");
+ 
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+     
+//        Página Preta
+        
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+    });
+ 
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+    
+     $("#scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
+    
+});
+
